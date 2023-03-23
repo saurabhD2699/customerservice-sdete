5b9a2471fb9d4985998214663d1994653834294d
Saurabh Dete <43199633+saurabhD2699@users.noreply.github.com>
2023-03-23 16:20:55 -0400

    Add files via upload

---
+++ customerservice/src/main/java/edu/iu/p565/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,53 @@
+package edu.iu.p565.customerservice.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import edu.iu.p565.customerservice.model.Customer;
+
+@Repository
+public class CustomerRepository {
+    
+    private List<Customer> customers = new ArrayList<>();
+
+    public List<Customer> findAll(){
+        return customers;
+    }
+
+    public int create(Customer customer){
+        int id = customers.size() + 1;
+        customer.setId(id);
+        customers.add(customer);
+        return id;
+    }
+
+    public void update(Customer customer, int id){
+        Customer x = getCustomerbyId(id);
+        if(x != null){
+            x.setName(customer.getName());
+            x.setEmail(customer.getEmail());
+        }
+        else {
+            throw new IllegalStateException(
+
+            );
+        }
+    }
+
+    public void delete(int id){
+        Customer x = getCustomerbyId(id);
+        if(x != null){
+            customers.remove(x);
+        }
+        else {
+            throw new IllegalStateException();
+        }
+    }
+
+    private Customer getCustomerbyId(int id){
+        return customers.stream().filter(x -> x.getId() == id).findAny().orElse(null);
+    }
+
+}
